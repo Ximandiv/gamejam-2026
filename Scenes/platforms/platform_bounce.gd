6e50f306dfb1dc1a69ab61f4df2ac1bc1e21156c
hard75 <barrazajesusarmando@gmail.com>
2026-01-31 14:26:36 -0500

    add bounce block and dropblock

--- Scenes/platforms/platform_bounce.gd
+++ Scenes/platforms/platform_bounce.gd
@@ -1,31 +1,25 @@
 extends StaticBody2D
 
-@export var fuerza_rebote: float = -100.0  # negativo = hacia arriba
+@export var fuerza_rebote: float = -175.0  # negativo = hacia arriba
 @export var area_2d: Area2D
 @export var limit_area: Area2D
 
-@onready var tween = create_tween()
-@export var bounce_quantity: int = 3
+@export var bounce_quantity: int = 4
 var current_bounce_quantity := 0
 
 func _ready():
 	area_2d.body_entered.connect(_on_body_entered)
-	limit_area.body_exited.connect(_on_body_exited)
-	limit_area.body_entered.connect(_on_limit_area)
-		
-func _on_limit_area(body: Node):
+	area_2d.body_exited.connect(_on_body_exited)
+
+func _on_body_exited(body: Node):
 	if body is CharacterBody2D \
 	and body.velocity.y == 0:
 		current_bounce_quantity = 0
 
-func _on_body_exited(body: Node):
-	print("body exited")
-	pass
-
 func _on_body_entered(body: Node) -> void:
-	print("entra")
 	if body is CharacterBody2D \
+	and body.velocity.y > 0 \
 	and current_bounce_quantity < bounce_quantity:
+		await get_tree().create_timer(0.1).timeout
 		body.velocity.y = fuerza_rebote
 		current_bounce_quantity += 1
-		
