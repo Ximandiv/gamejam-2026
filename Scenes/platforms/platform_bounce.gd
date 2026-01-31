d0a277fac3c4e3c7f32c60281003e3435f33c4c1
hard75 <barrazajesusarmando@gmail.com>
2026-01-31 11:19:35 -0500

    add and edit bounce platform

--- Scenes/platforms/platform_bounce.gd
+++ Scenes/platforms/platform_bounce.gd
@@ -2,11 +2,30 @@ extends StaticBody2D
 
 @export var fuerza_rebote: float = -100.0  # negativo = hacia arriba
 @export var area_2d: Area2D
+@export var limit_area: Area2D
+
+@onready var tween = create_tween()
+@export var bounce_quantity: int = 3
+var current_bounce_quantity := 0
 
 func _ready():
 	area_2d.body_entered.connect(_on_body_entered)
+	limit_area.body_exited.connect(_on_body_exited)
+	limit_area.body_entered.connect(_on_limit_area)
+		
+func _on_limit_area(body: Node):
+	if body is CharacterBody2D \
+	and body.velocity.y == 0:
+		current_bounce_quantity = 0
+
+func _on_body_exited(body: Node):
+	print("body exited")
+	pass
 
 func _on_body_entered(body: Node) -> void:
-	# Solo afecta al jugador
-	if body is CharacterBody2D:
-		body.velocity.y += fuerza_rebote
+	print("entra")
+	if body is CharacterBody2D \
+	and current_bounce_quantity < bounce_quantity:
+		body.velocity.y = fuerza_rebote
+		current_bounce_quantity += 1
+		
