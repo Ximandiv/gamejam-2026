9c77e459cda9841e169e6264ea574b2c2b0c8922
hard75 <barrazajesusarmando@gmail.com>
2026-01-30 23:27:59 -0500

    prueba player

--- Scripts/Player/PlayerMovement.gd
+++ Scripts/Player/PlayerMovement.gd
@@ -1,11 +1,51 @@
-extends Node
+extends CharacterBody2D
 
+signal personaje_muerto
 
-# Called when the node enters the scene tree for the first time.
+# @export var animation: AnimatedSprite2D
+@export var area_2d: Area2D
+
+var _velocidad: float = 100.0
+var _velocidad_salto: float = -300.0
+var _muerto: bool = false
+
 func _ready() -> void:
-	pass # Replace with function body.
+	add_to_group("personajes")
+	_muerto = false
+	area_2d.body_entered.connect(_on_area_2d_body_entered)
 
+func _physics_process(delta: float) -> void:
+	if _muerto:
+		return
+		
+	# gravedad
+	velocity += get_gravity() * delta
+	
+	# slalto
+	if Input.is_action_just_pressed("saltar") and is_on_floor():
+		velocity.y = _velocidad_salto
+	
+	#movimiento lateral
+	if Input.is_action_pressed("derecha"):
+		velocity.x = _velocidad
+		# animation.flip_h = true
+	elif Input.is_action_pressed("izquierda"):
+		velocity.x = -_velocidad
+		# animation.flip_h = false
+	else:
+		velocity.x = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	move_and_slide()
+	
+	# if !is_on_floor():
+		# animation.play("saltar")
+	# elif velocity.x != 0:
+		# animation.play("correr")
+	# else:
+		# animation.play("idle")
+		
+func _on_area_2d_body_entered(_body: Node2D):
+	_muerto = true
+	# animation.stop()
+	await get_tree().create_timer(0.5).timeout
+	personaje_muerto.emit()
