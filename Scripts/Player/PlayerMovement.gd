63515b9bcb056d1dbbae9c82046053a8d3e518f6
hard75 <barrazajesusarmando@gmail.com>
2026-01-31 00:07:54 -0500

    add player movement and camera

--- Scripts/Player/PlayerMovement.gd
+++ Scripts/Player/PlayerMovement.gd
@@ -2,11 +2,10 @@ extends CharacterBody2D
 
 signal personaje_muerto
 
-# @export var animation: AnimatedSprite2D
 @export var area_2d: Area2D
 
 var _velocidad: float = 100.0
-var _velocidad_salto: float = -300.0
+var _velocidad_salto: float = -350.0
 var _muerto: bool = false
 
 func _ready() -> void:
@@ -14,38 +13,26 @@ func _ready() -> void:
 	_muerto = false
 	area_2d.body_entered.connect(_on_area_2d_body_entered)
 
-func _physics_process(delta: float) -> void:
-	if _muerto:
-		return
-		
+func _physics_process(delta: float) -> void:		
 	# gravedad
 	velocity += get_gravity() * delta
 	
 	# slalto
-	if Input.is_action_just_pressed("saltar") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = _velocidad_salto
 	
 	#movimiento lateral
-	if Input.is_action_pressed("derecha"):
+	if Input.is_action_pressed("right"):
 		velocity.x = _velocidad
-		# animation.flip_h = true
-	elif Input.is_action_pressed("izquierda"):
+	elif Input.is_action_pressed("left"):
 		velocity.x = -_velocidad
-		# animation.flip_h = false
 	else:
 		velocity.x = 0
 
 	move_and_slide()
-	
-	# if !is_on_floor():
-		# animation.play("saltar")
-	# elif velocity.x != 0:
-		# animation.play("correr")
-	# else:
-		# animation.play("idle")
+
 		
 func _on_area_2d_body_entered(_body: Node2D):
 	_muerto = true
-	# animation.stop()
 	await get_tree().create_timer(0.5).timeout
 	personaje_muerto.emit()
