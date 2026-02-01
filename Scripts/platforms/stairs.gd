d0aae7fac09cd666f6691d9e03e889c65c7fe33b
hard75 <barrazajesusarmando@gmail.com>
2026-01-31 19:48:47 -0500

    add stair fall

---
+++ Scripts/platforms/stairs.gd
@@ -0,0 +1,8 @@
+extends Node2D
+@export var offset_time := 3.0
+
+func fall():
+	await get_tree().create_timer(offset_time).timeout
+	var children = get_children()
+	for child in children:
+		child.freeze = false
